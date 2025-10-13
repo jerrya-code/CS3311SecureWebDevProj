69f7184b6855256db58fc062f178200ed6ec5902
akkabakovb <akkabakov.bektur159@gmai.com>
2025-10-12 19:37:47 -0600

    Added Django functionality

--- MealPlanner/Meal_Planner/views.py
+++ MealPlanner/Meal_Planner/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def main(request):
+    return render(request, 'index.html')
+
+def drinks(request):
+    return render(request, 'drinks.html')
