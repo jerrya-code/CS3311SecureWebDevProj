82771dc68ee8ff0e8e898146ae3c92fd14c811ae
akkabakovb <akkabakov.bektur159@gmai.com>
2025-10-16 19:37:13 -0600

    Update

--- MealPlanner/Meal_Planner/models.py
+++ MealPlanner/Meal_Planner/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 class FoodCard(models.Model):
+    category = [
+        ('entries', 'Entries'),
+        ('salads', 'Salads'),
+        ('appetizers', 'Appetizers'),
+        ('desserts', 'Desserts'),
+        ('drinks', 'Drinks'),
+    ]
     image = models.ImageField(upload_to='foodCard_images/')
     title = models.CharField(max_length=100)
     description = models.TextField()
     proteins = models.CharField(max_length=20)
     fats = models.CharField(max_length=20)
     carbohydrates = models.CharField(max_length=20)
-    category = models.CharField(max_length=50, default='', blank=True)
+    category = models.CharField(max_length=50, choices=category, default='entries')
--- MealPlanner/Meal_Planner/urls.py
+++ MealPlanner/Meal_Planner/urls.py
@@ -3,9 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('menu/<str:category>/', views.category_view, name='category'),
-    # Optional shortcut for drinks category
-    path('drinks/', views.category_view, {'category': 'drinks'}, name='drinks'),
-    
-    path('desserts/', views.category_view, {'category': 'desserts'}, name='desserts'),
+    path('/<str:category>/', views.category_view, name='category'),
 ]
