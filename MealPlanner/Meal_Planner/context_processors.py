eead9b98542d52b2576803b6144835547d78c6ab
akkabakovb <akkabakov.bektur159@gmai.com>
2025-12-10 00:43:36 -0700

    Added image to the cart

--- MealPlanner/Meal_Planner/context_processors.py
+++ MealPlanner/Meal_Planner/context_processors.py
@@ -16,7 +16,10 @@ def view_cart(request):
         card = get_object_or_404(FoodCard, pk=int(cardpk))
         if card.category not in groupped_items:
             groupped_items[card.category] = {}
-        groupped_items[card.category][card.title] = quantity
+        groupped_items[card.category][card.title] = {
+            "quantity": quantity,
+            "image": card.image.url if card.image else None,
+        }
         totals['proteins'] += card.proteins * quantity
         totals['carbs'] += card.carbohydrates * quantity
         totals['fats'] += card.fats * quantity 
